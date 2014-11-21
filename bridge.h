f43ea6b8671609b4e4e1ac8f55bf42883c510a94
santoshsankar <santoshsankar@gmail.com>
2014-11-20 20:07:02 -0500

    first

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  PhotoSearchExample
+//
+//  Created by Santosh Sankar on 10/19/14.
+//  Copyright (c) 2014 Santosh Sankar. All rights reserved.
+//
+
+#ifndef PhotoSearchExample_bridge_h
+#define PhotoSearchExample_bridge_h
+
+
+#endif
